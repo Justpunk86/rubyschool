ca65d6090b6d1a2675a8a7a3f7fd71222e0e8a7c
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-09-10 18:22:07 +0500

    create rake migration

--- less29/app.rb
+++ less29/app.rb
@@ -1,12 +1,12 @@
 require "rubygems"
 require "sinatra"
 require "sinatra/reloader"
-require "sinatra-activerecord"
+require "sinatra/activerecord"
 
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	
+
 end
 
 get '/' do
