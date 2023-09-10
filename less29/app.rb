131f31ebe654a40687cd47607eae6d023ec70b1d
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-09-10 17:31:55 +0500

    add client entity

--- less29/app.rb
+++ less29/app.rb
@@ -1,6 +1,13 @@
 require "rubygems"
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra-activerecord"
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+	
+end
 
 get '/' do
 	erb 'Wellcom to Sinatra-Bootstrap Clean Template!'
