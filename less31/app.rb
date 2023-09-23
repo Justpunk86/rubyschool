8b5623edfe16e11c01c014fcb06c81d562ba0938
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-09-23 21:30:48 +0500

    do any features in pizzashop

--- less31/app.rb
+++ less31/app.rb
@@ -1,11 +1,60 @@
 require "rubygems"
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/activerecord"
+require "sqlite3"
+
+#set :database, "sqlite3:pizzashop.db"
+
+configure :default do 
+	set :database, 
+	{adapter: "sqlite3",  
+		#encoding: "unicode", 
+		database: "pizzashop.db", 
+		pool: 2
+		# username: "your_username", 
+		# password: "your_password"
+	}
+end
+
+configure :development do 
+	set :database, 
+	{adapter: "sqlite3",  
+		#encoding: "unicode", 
+		database: "pizzashop.db", 
+		pool: 2
+		# username: "your_username", 
+		# password: "your_password"
+	}
+end
+
+configure :production do 
+	set :database, 
+	{adapter: "sqlite3",  
+		#encoding: "unicode", 
+		database: "pizzashop.db", 
+		pool: 2
+		# username: "your_username", 
+		# password: "your_password"
+	}
+end
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
+	@products = Product.all
 	erb :index
 end
 
 get '/about' do
 	erb :about
 end	
+
+get '/menu' do
+	erb :menu
+end
+
+get '/orders' do
+	erb :menu
+end
--- less31/public/app.js
+++ less31/public/app.js
@@ -1,3 +1,13 @@
-	function something(){
-		alert('Hello everyone!');
-	}
+
+function something()
+{
+	var x = window.localStorage.getItem('aaa');
+	x = x*1 + 1;
+	window.localStorage.setItem('aaa', x);
+	alert(x);
+
+}
+
+function add_to_cart(id) {
+	alert("You added pizza with id: " + id);
+}
