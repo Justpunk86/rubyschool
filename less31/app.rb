ba6e74a6fd48a597f20a050aeaa5bdd6d776e774
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-09-30 23:38:22 +0500

    complete pizza-shop

--- less31/app.rb
+++ less31/app.rb
@@ -40,6 +40,36 @@ configure :production do
 end
 
 class Product < ActiveRecord::Base
+	belongs_to :order_details
+end
+
+class Order < ActiveRecord::Base
+	has_many :order_details
+end
+
+class OrderDetail < ActiveRecord::Base
+	belongs_to :orders
+	belongs_to :products
+end
+
+class FullOrderDetail < ActiveRecord::Base
+
+end
+
+def parse strin
+	arr_order_products = strin.split(',')
+	hh_orders = {}
+	
+	pizza_id = 0
+	pizza_qty = 0
+
+	arr_order_products.each do |item|
+		pizza_id = item.split('=')[0].split('_')[1].chomp.to_i
+		pizza_qty = item.split('=')[1]
+		hh_orders[pizza_id] = pizza_qty
+	end
+
+	return hh_orders
 end
 
 get '/' do
@@ -56,5 +86,63 @@ get '/menu' do
 end
 
 get '/orders' do
-	erb :menu
+		@full_order_details = FullOrderDetail.all
+	erb :orders
+end
+
+get '/cart' do
+	#@txt_cart_products = params[:orders]
+	
+	erb :cart
+end
+
+post '/cart' do
+	@txt_cart_products = params[:orders]
+	@hh_orders = parse @txt_cart_products
+	@pizza = Product.all
+
+	if @hh_orders.empty?
+		return erb :cart_is_empty
+	end
+
+	# @arr_order_products = @txt_cart_products.split(',')
+	
+	# pizza_id = 0
+
+	# @arr_order_products.each do |item|
+	# 	pizza_id = item.split('=')[0].split('_')[1].chomp.to_i
+	# 	pizza_qty = item.split('=')[1]
+	# 	@hh_orders[pizza_id] = pizza_qty
+
+	# end
+
+	@text_hh = ''
+	@hh_orders.each do |key,value|
+		@text_hh += "#{key}=#{value},"
+	end
+
+  erb :cart
+end
+
+post '/order' do
+
+	order = Order.create params[:client]
+
+	@hh_final = {}
+	final_order = params[:final_order]
+	arr_final = final_order.split(',')
+
+	arr_final.each do |item|
+		pizza_id = item.split('=')[0].to_i
+		pizza_qty = item.split('=')[1].to_i
+		@hh_final[pizza_id] = pizza_qty
+	end
+
+	@hh_final.each do |key,value|
+		order_details = OrderDetail.new :order_id => order.id, :product_id => key.to_i, :qty => value.to_i
+		order_details.save
+	end
+
+	#redirect to '/'
+	erb "Your order is having"
 end
--- less31/public/app.js
+++ less31/public/app.js
@@ -9,5 +9,63 @@ function something()
 }
 
 function add_to_cart(id) {
-	alert("You added pizza with id: " + id);
+	var key = 'product_' + id
+	var x = window.localStorage.getItem(key);
+	x = x*1 + 1;
+	window.localStorage.setItem(key, x)
+	upd_orders_input();
+	upd_orders_btn();
+}
+
+function get_total_qty() {
+	var total = 0;
+	var key;
+	for (var i = 0; i < localStorage.length; i++){
+		key = localStorage.key(i);
+		if (key.indexOf('product_') == 0){
+			total += localStorage.getItem(key)*1;
+		}
+	}
+	//$('body').append("Total qty pizzas added to card: " + total);
+	return total;
+}
+
+function get_cart_orders() {
+	var orders = '';
+	var key;
+	var value;
+	for (var i = 0; i < localStorage.length; i++){
+		key = localStorage.key(i);
+		value = localStorage.getItem(key);
+		if (key.indexOf('product_') == 0){
+			orders += key + '=' + value + ',';
+		}
+	}
+
+	return orders;
+}
+
+function upd_orders_input() {
+	var orders = get_cart_orders();
+	$('#orders_input').val(orders);
+}
+
+function upd_orders_btn() {
+	var text = 'Cart (' + get_total_qty() + ')';
+	$('#orders_btn').val(text);
+}
+
+function empty_cart() {
+	window.localStorage.clear();
+	/*for (var i = 0; i < localStorage.length; i++){
+		key = localStorage.key(i);
+		if (key.indexOf('product_') == 0){
+			window.localStorage.removeItem(key);
+		}
+	}*/
+	upd_orders_input();
+	upd_orders_btn();
+	
+	$('#cart').text('Your cart is now empty');
+	return false;
 }
