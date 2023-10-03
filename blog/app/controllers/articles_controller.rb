7125bb26c11f9f448bbc06589421c102faa31293
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-03 17:45:08 +0500

    add features create, show, edit, delete articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,11 +1,54 @@
 class ArticlesController < ApplicationController
 
-  def new
+  def index
+    @articles = Article.all
+  end
 
+  def new
+    #@article = Article.new
   end
 
   def create
-    render plain: params[:article].inspect
+#    render plain: params[:article].inspect
+    @article = Article.new article_params
+
+    if @article.valid?
+      @article.save
+      redirect_to new_article_path
+    else
+      render action: "new"
+    end
+    
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id]) 
+    article.update article_params
+    article.save
+
+    redirect_to edit_article_path
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.delete
+    article.save
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 
