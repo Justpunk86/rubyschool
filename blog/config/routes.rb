d467cef6d615ec0686b3ba5fcee2f673c439579b
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-03 22:32:55 +0500

    create comments

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   #get '/contacts' => 'contacts#index'
   #post '/contacts' => 'contacts#create'
   #get '/contacts/new' => 'contacts#new'
-
-  resource :contacts, only: [:new, :create], path_names: { :new => '' }
-  #get '/articles' => 'articles#index'
-  resources :articles
   get '/about' => 'about#about'
   get '/terms' => 'terms#terms'
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
+  #get '/articles' => 'articles#index'
+  resources :articles  do
+    resources :comments, only: [:create, :edit, :delete]
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
