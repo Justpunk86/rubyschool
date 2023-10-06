25c075c8f389f1966f3058d025246cf20b82f13d
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-06 09:53:18 +0500

    displaing user on page

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  #delete '/users/sign_out' => 'devise/sessions#destroy'
   root to: "home#index"
   get '/' => 'home#index'
   #get 'home/index'
