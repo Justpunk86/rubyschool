111105e9d6d8aed80700ffe89b21384253285c7a
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-16 18:11:04 +0500

    add factory bot

--- blog/spec/models/article_spec.rb
+++ blog/spec/models/article_spec.rb
@@ -9,5 +9,14 @@ describe Article, type: :model do
   describe "associations" do
     it { should have_many(:comments)}
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      #create object Aricle another variant
+      aricle = create(:article, title: 'Lorem Ipsum')
+
+      expect(aricle.subject).to eq 'Lorem Ipsum'
+    end
+  end     
 end
   
