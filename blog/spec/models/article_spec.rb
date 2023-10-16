f197f13756384e90fae6a8470078c419c889e591
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-16 17:00:35 +0500

    add test article

--- blog/spec/models/article_spec.rb
+++ blog/spec/models/article_spec.rb
@@ -1,6 +1,13 @@
 require "spec_helper"
 
 describe Article, type: :model do
-  it { should have_many(:comments)}
+  describe "validations" do
+    it { should validate_presence_of(:text)}
+    it { should validate_presence_of(:title)}
+  end 
+
+  describe "associations" do
+    it { should have_many(:comments)}
+  end
 end
   
