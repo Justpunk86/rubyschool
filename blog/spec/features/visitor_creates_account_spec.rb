349485f56c2db7db79bc5c35c179653914466c05
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-19 21:17:35 +0500

    add test for page create account

---
+++ blog/spec/features/visitor_creates_account_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+  feature "Contact Creation" do 
+    scenario "allows guest to create account" do
+      visit new_user_registration_path
+
+      fill_in :user_email, :with => 'guest@example.com'
+      fill_in :user_username, :with => 'temp'
+      fill_in :user_password, :with => '123456'
+      fill_in :user_password_confirmation, :with => '123456'
+
+      click_button 'Sign up'
+
+      expect(page).to have_content I18n.t('devise.registrations.signed_up')
+
+  end 
+end
