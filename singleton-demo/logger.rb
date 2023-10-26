06411b469b40bf1d12b114199060ed05d539ced1
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2023-10-26 17:49:18 +0500

    create singleton-demo

--- singleton-demo/logger.rb
+++ singleton-demo/logger.rb
@@ -1,21 +1,26 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
+  # @@x = Logger.new
 
-  def self.instance
-    return @@x
-  end
+  # def self.instance
+  #   return @@x
+  # end
 
-  def self.say_something
-    puts 'haha'
-  end
+  # def self.say_something
+  #   puts 'haha'
+  # end
 
   def log_something wat
     @f.puts wat
+    @f.flush
   end
 
-  private_class_method :new
+  # private_class_method :new
 end
